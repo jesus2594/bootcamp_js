20bd9a7f1bff745cc12203e4340259dee739026c
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-25 11:33:46 -0500

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+/* 
+En este ejercicio tienes que crear un archivo JavaScript donde crees una variable la cual contenga una cadena de texto.
+
+Después crearás una función, dentro de ella crearas una variable que se llame igual que la primera que has creado y le asignaras una cadena de texto diferente a la primera y mostrarás la variable por consola.
+
+Por último, fuera de la función tendrás que realizar la llamada a la función y mostrarás por consola la primera variable que has creado.
+ */
+let str = "Hola Mundo"
+
+function cadena(){
+   let str = "Hola open-bootcamp"
+   console.log(str)
+}
+
+cadena()
+console.log(str)
