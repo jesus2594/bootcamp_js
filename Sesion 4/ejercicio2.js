4adcc43f3413963a287ac96c8686e6401dff1fa7
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-26 22:41:01 -0500

    Sesion 4 ejercicio 2

---
+++ Sesion 4/ejercicio2.js
@@ -0,0 +1,3 @@
+//Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.
+let str = "eJercicio de strinGs en javaScripT"
+console.log(str.toUpperCase())
