98c40b0be770e5a9ab155ae0e7c2b58ae0fbc844
Jesus Marmol <jesusdavid890@hotmail.com>
2022-05-12 14:25:07 -0500

    React Ejercicio Sesion 1, 2 y 3

---
+++ src/components/container/task_list.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { LEVELS } from '../../models/levels.enum'
+import { Task } from '../../models/task.class'
+import TaskComponent from '../pure/task'
+
+
+const TaskListComponent = () => {
+
+    const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL)
+
+    const changeState = id => {
+        
+    }
+
+  return (
+    <div>
+        <div>
+            Your Tasks:
+        </div>
+        <TaskComponent task={defaultTask}></TaskComponent>
+    </div>
+  )
+}
+
+
+
+export default TaskListComponent
