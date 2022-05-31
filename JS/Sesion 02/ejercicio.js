32a50e1a260c609652403df1f7a97a91d388b3b6
Jesus Marmol <jesusdavid890@hotmail.com>
2022-05-31 08:28:54 -0500

    Sesion 2 Ejercicio Actualizado

---
+++ JS/Sesion 02/ejercicio.js
@@ -0,0 +1,16 @@
+/* 
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+ */
+
+let lista = ["Jesus", 28, true, "25/04/1994", {"titulo": "Javascript", "autor": "web", "fecha": "14/07/1995", "url": "https://campus.open-bootcamp.com/"} ]
+
