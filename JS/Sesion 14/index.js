8b764cdde63d55637f9ffc6931a4ae9b695a3ba5
Jesus Marmol <jesusdavid890@hotmail.com>
2022-06-05 11:07:39 -0500

    Sesion 14 Ejercicio Actualizado

---
+++ JS/Sesion 14/index.js
@@ -0,0 +1,11 @@
+
+
+let boton = document.querySelector('button')
+
+boton.addEventListener("click", () => alert("click en el botón"))
+
+alerta = alert("click en el botón")
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
--- JS/Sesion_14/public/controllers/nombres.js
+++
@@ -1,9 +0,0 @@
-    const arr = ["alfonso", "Manuel", "Diego", "gilberto"];
-   
-   var nombres = arr.map(function(nombre){
-    return '<li>' + nombre.toString() + '</li>'
-  })
-  document.getElementById("nombres").innerHTML = nombres;
-  
-
-   
