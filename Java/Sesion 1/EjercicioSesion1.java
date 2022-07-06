893702d37f4ae5c959cdef4414faafafbea16bdb
Jesus Marmol <jesusdavid890@hotmail.com>
2022-07-06 11:48:00 -0500

    Ejercicio Sesion 1

---
+++ Java/Sesion 1/EjercicioSesion1.java
@@ -0,0 +1,18 @@
+
+package ejerciciosesion1;
+
+
+public class EjercicioSesion1 {
+    
+    
+    
+    public static void main(String[] args) {
+        int decimal = 100;
+        long decimal2 = 178823411L;
+        double flotante = 5.0;
+        boolean trueOrFalse = true;
+        String cadena = "Hola Sesion 1";
+        System.out.println("El int es " + decimal+ " el long es " + decimal2 + " el flotante es " + flotante + " el boolean es " + trueOrFalse+ " la cadena es " +cadena);
+    }
+    
+}
